30b1bbaf902f4790a47476360042acd8f7347254
Tharunbaikani <112890104+Tharunbaikani@users.noreply.github.com>
2025-01-02 16:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ input_data_scaled=scaler.transform(input_data)
 
 prediction=model.predict(input_data_scaled)
 prediction_proba=prediction[0][0]
+st.write(prediction_proba)
 
 if prediction_proba>0.5:
     st.write("Customer is likely to Churn.")
